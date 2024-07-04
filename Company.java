61524297021941a68febeb719d70194233d15478
Ashwin A <119737155+Ashwinmic@users.noreply.github.com>
2024-07-04 19:30:03 +0530

    Add files via upload

---
+++ Company.java
@@ -0,0 +1,19 @@
+package org.testing.login;
+
+import org.testing.practice.Hello;
+import org.testing.practice.Worker;
+
+public class Company {
+public void compId() {
+	// TODO Auto-generated method stub
+System.out.println("company id is 745384");
+}
+public static void main(String[] args) {
+	Company a=new Company();
+	a.compId();
+	Hello n=new Hello();
+	n.empId();
+	Worker r=new Worker();
+	r.workerId();
+}
+}
---
+++ Hello.java
@@ -0,0 +1,16 @@
+package org.testing.practice;
+
+public class Hello {
+ public void empId() {
+	System.out.println("Employee id is 332");
+}
+ public void empName() {
+	// TODO Auto-generated method stub
+System.out.println("Employee name is Arun");
+}
+public static void main(String[] args) {
+	Hello n=new Hello();
+	n.empId();
+	n.empName();
+}
+}
